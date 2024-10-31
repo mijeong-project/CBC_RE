23d5c6ecdb7233f07fa1913389113c6997badac8
mijeong-project <alwjd6405@naver.com>
2024-10-31 17:48:28 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// 상단메뉴고정
+
 $(function () {
     $(document).ready(function () {
 
@@ -12,4 +14,22 @@ $(function () {
             }
         });
     });
+});
+
+// swiper
+var swiper = new Swiper('.gallery .gallery_inner ', {
+    slidesPerView: 2.5,
+    spaceBetween: 0,//갤러리 사이 간격
+    speed: 1500,//버튼을 슬라이드가 넘어가는 시간
+    autoplay: {
+        delay: 2500,//자동으로 넘어가기 전 머무르는 시간
+        disableOnInteraction: false,
+    },
+    loop: true,//슬라이드 무한반복
+    navigation: {//화살표 버튼
+        nextEl: '.gallery .swiper-button-next',
+        prevEl: '.gallery .swiper-button-prev',
+    },
+    centeredSlides: true,
+    
 });
